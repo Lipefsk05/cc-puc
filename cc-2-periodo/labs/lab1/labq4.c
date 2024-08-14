604e051966412b7bf598cf5a6a846859a9c0d2cd
Felipe Portes Antunes <felipefenske05.com>
2024-08-14 15:42:12 -0300

    att

--- cc-2-periodo/labs/lab1/labq4.c
+++ cc-2-periodo/labs/lab1/labq4.c
@@ -3,29 +3,32 @@
 #include <stdbool.h>
 #include <ctype.h>
 
-int maimin(int i, char *stg){
+int maimin(int i, char *stg, int mai){
     
-    int mai=0;
-    
-    int tam = strlen(stg)-1;
+    int tam = strlen(stg);
 
     if(i>=tam){
-        return mai;
-    }else if((stg[i]>=65 && stg[i]<=90)){
-        return maimin(i+1, stg)
-    }
 
+        return mai;
+
+    }else if((stg[i]>=65 && stg[i]<=90)){
+
+        return maimin(i+1, stg, mai+1);
+    }else 
+        return maimin (i+1, stg, mai);
 }
 
 int main(){ 
 
     char stg[1000];
-
+    int mai=0;
     
     do{
         scanf(" %[^\n]", stg);
+
         if(strcmp(stg, "FIM") != 0)
-        printf("%i\n", maimin(0, stg));
+   
+        printf("%i\n", maimin(0, stg, 0));
     }
 
     while(strcmp(stg, "FIM") != 0);
